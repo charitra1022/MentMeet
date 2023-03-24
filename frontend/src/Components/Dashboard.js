79306dbb0147f9dadf3471b97c5c5d0392def2a9
Yogesh Patel <yogesh.2022ca115@mnnit.ac.in>
2023-03-25 00:08:30 +0530

    Added dashboard

---
+++ frontend/src/Components/Dashboard.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import NavbarComponent from "./Navbar"
+import { Badge, Container } from 'react-bootstrap'
+import profile from './profile.png'
+import './style/Dashboard.css';
+
+export default function Dashboard() {
+    return (
+        <>
+            <NavbarComponent />
+
+            <Container >
+                <div >
+                    <div className="ProfileimgContainer text-center">
+
+                        {/* Profile image here */}
+                        <img class="roundedCircle profileimg" src={profile}></img>
+
+                    </div>
+                    <div className="DetailContainer text-center">
+
+                        {/* User name will come here */}
+                        <h1 className="heading">Ayush Rai</h1>
+
+                        <div className="badgebox">
+
+                            {/* badges will come here */}
+                            <Badge bg="dark">
+                                JAVA
+                            </Badge>{' '} {/* this must includde */}
+                        </div>
+
+                        {/* Discription will come here */}
+                        <div id="Discription" className="text-center">
+                            Hello, I am software engeneer at TCS, having four years of experience in tech industry
+                            and a mentor. I have gueded more than hundred individuals.
+                        </div>
+
+                        <div className="footer">
+
+                            {/* Email will comehere */}
+                            <span id="email">
+                                Email : <b>ayush2022ca017@mnnit.ac.in</b>
+                            </span> <br />
+
+                            {/* Phone number will come here */}
+                            <span id="phone">
+                                Phone: <b>1234567890</b>
+                            </span>
+
+                        </div>
+                    </div>
+                </div>
+            </Container>
+        </>
+    )
+}
