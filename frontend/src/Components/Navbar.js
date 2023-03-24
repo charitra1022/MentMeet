bc661ed56c57700db50cefca81d3f94801966c3e
Charitra Agarwal <charitraagarwal1022@gmail.com>
2023-03-24 23:55:26 +0530

    modified navbaf

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -1,21 +1,23 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
-import "./NavbarStyle.css";
+import "./styles/NavbarStyle.css";
 
 export default function NavbarComponent() {
-    return (
-        <Navbar className="navcontainer">
-            <Container >
-                <Navbar.Brand href="#home"><h2>MentiMeet</h2></Navbar.Brand>
-                <Navbar.Toggle />
-                <Navbar className="justify-content-end">
-                    <Navbar.Text>
-                        <button className="chatbtn">Chat</button>
-                        <button className="logoutbtn">Logout</button>
-                    </Navbar.Text>
-                </Navbar>
-            </Container>
+  return (
+    <Navbar className="navcontainer">
+      <Container>
+        <Navbar.Brand href="#home">
+          <h2>MentMeet</h2>
+        </Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar className="justify-content-end">
+          <Navbar.Text>
+            <button className="chatbtn">Chat</button>
+            <button className="logoutbtn">Logout</button>
+          </Navbar.Text>
         </Navbar>
-    );
+      </Container>
+    </Navbar>
+  );
 }
